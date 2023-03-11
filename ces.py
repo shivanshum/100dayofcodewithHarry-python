075492d15c9751b1140611dafa87fc33b9f12948
SHIVANSHU MISHRA <53150676+shivanshum@users.noreply.github.com>
2023-03-11 15:29:23 +0000

    pip

---
+++ ces.py
@@ -0,0 +1,7 @@
+print("Hey I am a good boy! \nand this viewer is also a good girl/Boy")
+# commented this line
+print("Hey!" ,6,7)
+print("Hey!" ,6,7, sep="~")
+print("\n")
+print("Hey", 6, 7, sep="~", end="009")
+print()
---
+++ datatypes.py
@@ -0,0 +1,12 @@
+a = 1
+print(a)
+b ="SHIVA"
+print(b)
+print("The Type of a is", type(a))
+print("The Type of b is", type(b))
+list = [4,5,8,2.3,1]
+print(list)
+tuple= ("Apple","Banana","Coconut","Dish")
+print(tuple)
+dict = {"Name":"Shivanshu","Age":"23"}
+print(dict)
