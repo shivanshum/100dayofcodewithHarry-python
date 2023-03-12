936ee7fb56d8ff820ccec1cba59d7424f05711ec
SHIVANSHU MISHRA <53150676+shivanshum@users.noreply.github.com>
2023-03-12 06:46:25 +0000

    pip

---
+++ sstring.py
@@ -0,0 +1,15 @@
+name = "SHIVA"
+friend ="ishant"
+anotherfriend = 'madan'
+fruit = 'I want to eat Apple'
+print("Hello," + name)
+# print(fruit)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(len(name))
+
+for character in fruit:
+  print(character)
---
+++ sstrings.py
@@ -0,0 +1,10 @@
+# names = "Hell0, SHIVA!"
+# fruit = "Mango"
+# print(names[0:5])
+# print(len(names))
+fruit = "MANgo"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:3])
+print(fruit[1:4])
+print(fruit[:])
